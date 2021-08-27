f3ee10f25a34b155852ca0df3d07753fe1adf81c
Jernej Zorec <jernejz@gmail.com>
2021-08-27 20:23:45 +0200

    Initial SPM file

---
+++ Sources/DMNumberSpellOutFormatter/DMNumberSpellOutFormatter.swift
@@ -0,0 +1,3 @@
+struct DMNumberSpellOutFormatter {
+    var text = "Hello, World!"
+}
---
+++ Tests/DMNumberSpellOutFormatterTests/DMNumberSpellOutFormatterTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import DMNumberSpellOutFormatter
+
+    final class DMNumberSpellOutFormatterTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(DMNumberSpellOutFormatter().text, "Hello, World!")
+        }
+    }
